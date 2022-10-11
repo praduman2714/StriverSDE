45fc623b1a11151ba268f2f0d3b07e4684c1b437
praduman2714 <pradu14jsr@gmail.com>
2022-10-11 11:12:45 +0530

    LinkedList1

---
+++ Day5/MiddleOfTheLinkedList.java
@@ -0,0 +1,29 @@
+The first approach is that we count the number of node present in the 
+linked List, and then with the help of one dummy node traverse the 
+linked list n/2 and return the dummy node.
+
+The optimal approach is Hare And The Tortoise method
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+        }
+        return slow;
+    }
+}
