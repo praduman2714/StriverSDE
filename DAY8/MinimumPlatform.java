f66ec26095ebb221856e74ab9e3665ce6861fa68
praduman2714 <pradu14jsr@gmail.com>
2022-10-14 11:45:03 +0530

    Greedy

---
+++ DAY8/MinimumPlatform.java
@@ -0,0 +1,24 @@
+The brute force would be that if we can count the interval between the two timining, it would take n2
+
+The optiamal approach is using sorting
+
+int plat_needed = 1, result = 1; 
+        int i = 1, j = 0; 
+  
+        while (i < n && j < n) { 
+        
+            if (arr[i] <= dep[j]) { 
+                plat_needed++; 
+                i++; 
+            } 
+  
+            else if (arr[i] > dep[j]) { 
+                plat_needed--; 
+                j++; 
+            } 
+  
+            if (plat_needed > result) 
+                result = plat_needed; 
+        } 
+  
+        return result;
