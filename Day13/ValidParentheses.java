7caa92d64c40e41fb34f6c49d30b09d0b5c21611
praduman2714 <pradu14jsr@gmail.com>
2022-10-24 16:04:11 +0530

    Stack

---
+++ Day13/ValidParentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch == '[' || ch == '(' || ch == '{'){
+                st.push(ch);
+            }else if(ch == ']' && !st.isEmpty() && st.peek() == '['){
+                st.pop();
+            }else if(ch == ')' && !st.isEmpty() && st.peek() == '('){
+                st.pop();
+            }else if(ch == '}' && !st.isEmpty() && st.peek() == '{' ){
+                st.pop();
+            }else{
+                return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
+
+Time complexity : O(N)
+Space Complexity : O(N)
