8f96d0a95b125fab67163432cc0db8e91982f2d9
praduman2714 <pradu14jsr@gmail.com>
2022-10-27 15:28:02 +0530

    string:

---
+++ Day15/RomanToInteger.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        
+        int n = s.length();
+        int sum = map.get(s.charAt(n-1));
+        for(int i = n-2; i>= 0; i--){
+            int curr = map.get(s.charAt(i));
+            int next = map.get(s.charAt(i+1));
+            
+            if(next > curr){
+                sum-= curr;
+            }else{
+                sum+=curr;
+            }
+        }
+        return sum;
+    }
+}
