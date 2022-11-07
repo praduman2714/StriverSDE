0d1975cd5c8b4fcd7690861294cc1946a1035416
praduman2714 <pradu14jsr@gmail.com>
2022-11-07 12:40:14 +0530

    BST

---
+++ Day20/SearchInABinaryTree.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        while(root != null){
+            if(root.val == val){
+                return root;
+            }else if(root.val < val){
+                root = root.right;
+            }else{
+                root = root.left;
+            }
+        }
+        return root;
+        
+    }
+}
+
+the next one is the recursive approach
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null) return null;
+        
+        if(root.val == val) return root;
+        
+        if(root.val >  val){
+            return searchBST(root.left, val);
+        }
+        
+        return searchBST(root.right, val);
+    }
+}
